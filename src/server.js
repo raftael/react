e076264d1319a4f08b5fb5bd194c37f908d8018d
Rafael Roman Garcia <BMXDDT005616@bdt.lan>
2017-02-21 15:31:03 -0500

    static public folder

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const express = require('express')
 
 module.exports = {
   app: function () {
-    const app = express()
-    const indexPath = path.join(__dirname, '../index.html')
-    const publicPath = express.static(path.join(__dirname, 'public'))
-
-    app.use('/public', publicPath)
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
+    const app = express();
+    const indexPath = path.join(__dirname, '../index.html');
+    //const publicPath = express.static(path.join(__dirname, 'public'))
+    //app.use('/public', publicPath)
+    app.use("/public", express.static(path.join(__dirname,'public')));
+    app.get('/', function (_, res) { res.sendFile(indexPath) });
 
     return app
   }
