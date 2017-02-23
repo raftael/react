ed9fa0d30df80a303a75b03c98197c413f9f7764
Rafael Roman Garcia <BMXDDT005616@bdt.lan>
2017-02-23 00:08:14 -0500

    update procfile

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ module.exports = {
     const indexPath = path.join(__dirname, '../index.html');
     //const publicPath = express.static(path.join(__dirname, 'public'))
     //app.use('/public', publicPath)
-    app.use("/public", express.static(path.join(__dirname,'public')));
+    app.use("../public", express.static(path.join(__dirname,'public')));
     app.get('/', function (_, res) { res.sendFile(indexPath) });
 
     return app
